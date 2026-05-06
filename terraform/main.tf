7aa6b5d089586719e009b5447987f796e67d93c4
montibrunomiguel <montibrunomiguel@gmail.com>
2026-05-05 21:47:29 -0300

    fix error

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,6 @@ resource "aws_s3_bucket" "example" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
 
-  block_public_acls   = false
-  block_public_policy = false
+  block_public_acls   = true
+  block_public_policy = true
 }
