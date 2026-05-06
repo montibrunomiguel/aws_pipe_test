74a6244c589e208d600a45888ba735b9a55bc363
montibrunomiguel <montibrunomiguel@gmail.com>
2026-05-05 21:49:25 -0300

    solve error

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,5 @@ resource "aws_s3_bucket" "example" {
 # CONFIGURAÇÃO INSEGURA PROPOSITAL
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
-
   block_public_acls   = true
-  block_public_policy = true
 }
