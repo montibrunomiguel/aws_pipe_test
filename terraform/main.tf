a5ed18bb25363487493c33cf5fb475cb1963c2dd
montibrunomiguel <montibrunomiguel@gmail.com>
2026-07-07 23:49:24 -0300

    refactor: update S3 bucket configuration for Trivy testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,20 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "meu-bucket-exemplo-123456"
-
-  tags = {
-    Name = "Test_bucket_monti"
-  }
+# Bucket S3 intencionalmente mal configurado para testar o Trivy
+resource "aws_s3_bucket" "vulnerable_bucket" {
+  bucket = "meu-bucket-com-falhas-de-seguranca-2026"
 }
 
-# CONFIGURAÇÃO INSEGURA PROPOSITAL
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
-  block_public_acls   = true
+# Falha 1: Tornando o bucket público explicitamente
+resource "aws_s3_bucket_public_access_block" "bad_practice" {
+  bucket = aws_s3_bucket.vulnerable_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Falha 2: ACL pública (Antiga, mas o Trivy ainda pega)
+resource "aws_s3_bucket_acl" "bad_acl" {
+  bucket = aws_s3_bucket.vulnerable_bucket.id
+  acl    = "public-read"
 }
